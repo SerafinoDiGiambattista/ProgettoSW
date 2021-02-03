68a9f7647d97bc72b54f644ca9331bf73e5cfb5e
Jacopo-Inglese <74725917+Jacopo-Inglese@users.noreply.github.com>
2021-02-03 21:48:23 +0100

    Update ParameterInvalidException.groovy

--- Tesiplugin/buildSrc/src/main/groovy/ParameterInvalidException.groovy
+++ Tesiplugin/buildSrc/src/main/groovy/ParameterInvalidException.groovy
@@ -1,6 +1,6 @@
 import groovy.transform.CompileStatic
 
-@CompileStatic
+ 
 class ParameterInvalidException extends Exception{
     ParameterInvalidException(String message){
         super(message)
