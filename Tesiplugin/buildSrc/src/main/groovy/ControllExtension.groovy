2910ec79b11de1b483c1d229c18368b91500cb3c
Jacopo-Inglese <74725917+Jacopo-Inglese@users.noreply.github.com>
2021-02-03 21:47:54 +0100

    Update ControllExtension.groovy

--- Tesiplugin/buildSrc/src/main/groovy/ControllExtension.groovy
+++ Tesiplugin/buildSrc/src/main/groovy/ControllExtension.groovy
@@ -2,8 +2,7 @@ import groovy.transform.CompileStatic
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
 import javax.inject.Inject
-
-@CompileStatic
+ 
 class ControllExtension {
 
     final Property<File> file1
