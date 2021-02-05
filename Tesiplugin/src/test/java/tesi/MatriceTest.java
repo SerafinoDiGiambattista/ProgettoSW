c66d806546d48f8b452a908bfe437c54f75ddb04
Jacopo-Inglese <74725917+Jacopo-Inglese@users.noreply.github.com>
2021-02-05 12:20:55 +0100

    Update MatriceTest.java

--- Tesiplugin/src/test/java/tesi/MatriceTest.java
+++ Tesiplugin/src/test/java/tesi/MatriceTest.java
@@ -3,7 +3,7 @@ package tesi;
 
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
-import org.junit.jupiter.api.BeforeEach;
+ 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,11 +12,6 @@ import java.util.Scanner;
 
 public class MatriceTest {
     
-@BeforeEach
-void setUp() throws Exception{
-        System.setProperty("java.awt.headless", "true");
-
-        }
     
     private ArrayList<Document> ndoc = new ListDocument().getNdoc();
 
