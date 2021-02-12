d6e6a25e778e5a9089d85c2aa447882056295c2f
Jacopo-Inglese <74725917+Jacopo-Inglese@users.noreply.github.com>
2021-02-12 17:15:05 +0100

    Delete Tesiplugin/buildSrc directory

--- Tesiplugin/buildSrc/src/main/groovy/pluginTesi/ExceedingLenghtValues.groovy
+++
@@ -1,9 +0,0 @@
-package pluginTesi
-
-import groovy.transform.CompileStatic
-
-class ExceedingLenghtValues  extends Exception{
-    ExceedingLenghtValues(String message){
-        super(message)
-    }
-}
