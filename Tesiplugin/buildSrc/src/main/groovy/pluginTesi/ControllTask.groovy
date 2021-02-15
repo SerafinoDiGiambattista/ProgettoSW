21ed2900b27e4ada987a6551b8424f1455432f29
Jacopo-Inglese <74725917+Jacopo-Inglese@users.noreply.github.com>
2021-02-15 17:25:24 +0100

    Update ControllTask.groovy

--- Tesiplugin/buildSrc/src/main/groovy/pluginTesi/ControllTask.groovy
+++ Tesiplugin/buildSrc/src/main/groovy/pluginTesi/ControllTask.groovy
@@ -7,8 +7,9 @@ import javax.swing.JTable
 import org.codehaus.groovy.control.messages.WarningMessage
 import java.io.Serializable
 
-class ControllTask extends DefaultTask  {
-@InputFile
+class ControllTask extends DefaultTask implements Serializable {
+ private static final long serialVersionUID =  3742318830738515599L;
+	@InputFile
 final Property<File> file2 = project.objects.property(File)
 
 
