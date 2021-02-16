0e744a13f38018b8d513573a7c58da9e5aa41c1b
Jacopo-Inglese <74725917+Jacopo-Inglese@users.noreply.github.com>
2021-02-16 19:20:02 +0100

    Update RfcTest.java

--- Tesiplugin/src/test/java/tesi/RfcTest.java
+++ Tesiplugin/src/test/java/tesi/RfcTest.java
@@ -8,24 +8,23 @@ import java.util.HashMap;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RfcTest {
-    private  static Rfc rfc;
-    private static HashMap<String, Integer> mappa = new HashMap<String,Integer>(); ;
+        private  static Rfc rfc;
+        private static HashMap<String, Integer> mappa = new HashMap<String,Integer>(); ;
 
-    @BeforeAll
-    static void setUpBeforeClass(){
+        @BeforeAll
+        static void setUpBeforeClass(){
+                rfc = new Rfc(new ListDocument().getNdoc());
+                rfc.calcolo();
 
+                mappa.put("CollabHistoryPresenter",54);
+                mappa.put("MaterialReferenceManager",53);
+                mappa.put("TMP_Text",1130);
+        }
 
-        rfc = new Rfc(new ListDocument().getNdoc());
-        rfc.calcolo();
+        @Test
+        void testCalcoloRfc(){
 
+                assertTrue(mappa.equals(rfc.getMap()));
+        }
 
-        mappa.put("CollabHistoryPresenter",54);
-        mappa.put("MaterialReferenceManager",53);
-        mappa.put("TMP_Text",1130);
-    }
-
-    @Test
-    void TestCalcoloRfc(){
-    assertTrue(mappa.equals(rfc.getMap()));
-    }
 }
