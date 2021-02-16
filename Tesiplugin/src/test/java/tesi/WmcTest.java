2542f01c0b51b518a686152ed92f2ac94c5d016a
Jacopo-Inglese <74725917+Jacopo-Inglese@users.noreply.github.com>
2021-02-16 19:20:20 +0100

    Update WmcTest.java

--- Tesiplugin/src/test/java/tesi/WmcTest.java
+++ Tesiplugin/src/test/java/tesi/WmcTest.java
@@ -7,26 +7,22 @@ import java.util.HashMap;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WmcTest {
-private static Wmc wmc;
-private static HashMap<String,Integer> mappa = new HashMap<String,Integer>();
+        private static Wmc wmc;
+        private static HashMap<String,Integer> mappa = new HashMap<String,Integer>();
 
-    @BeforeAll
-    static void setUpBeforeClass() {
+        @BeforeAll
+        static void setUpBeforeClass() {
+                wmc=new Wmc(new ListDocument().getNdoc());
+                wmc.calcolo();
 
-        wmc=new Wmc(new ListDocument().getNdoc());
-        wmc.calcolo();
+                mappa.put("CollabHistoryPresenter",22);
+                mappa.put("MaterialReferenceManager",13);
+                mappa.put("TMP_Text",1078);
+        }
 
-        mappa.put("CollabHistoryPresenter",22);
-        mappa.put("MaterialReferenceManager",13);
-        mappa.put("TMP_Text",1078);
-
-    }
-    @Test
-    void TestCalcoloWmc(){
-        assertTrue(mappa.equals(wmc.getMappa()));
-    }
-
-
-
+        @Test
+        void testCalcoloWmc(){
+                assertTrue(mappa.equals(wmc.getMappa()));
+        }
 
 }
